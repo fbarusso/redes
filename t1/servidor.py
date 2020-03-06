de46f123374328367024410d42a148115f45269f
Felipe Alves Barusso <falvesbarusso@gmail.com>
2020-03-06 13:34:47 -0300

    t1 - polishing no servidor

--- t1/servidor.py
+++ t1/servidor.py
@@ -1,93 +1,96 @@
-import socket 
-import select 
-import sys 
+import socket
+import select
+import sys
 import _thread
 
-# Configuracoes do socket
-servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-servidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# Configuracoes do socket.
+socketDoServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socketDoServidor.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Checa se a quantidade de parametros esta correta
+# Checa se os parametros estao corretos.
 if len(sys.argv) != 3:
-	print("Numero de parametros incorreto. Utilize servidor ip porta")
+	print("Parametros incorretos. Utilize servidor enderecoDoServidor portaDoServidor")
 	exit()
 
-# enderecoIP = primeiro argumento
-enderecoIP = str(sys.argv[1])
+# Primeiro agumento -> enderecoDoServidor. Segundo argumento -> portaDoServidor.
+enderecoDoServidor = str(sys.argv[1])
+portaDoServidor = int(sys.argv[2])
 
-# porta = segundo argumento
-porta = int(sys.argv[2])
+# Liga o socket do servidor a tupla (enderecoDoServidor, portaDoServidor).
+socketDoServidor.bind((enderecoDoServidor, portaDoServidor))
 
-# Liga o servidor a tupla (enderecoIP, porta)
-servidor.bind((enderecoIP, porta))
+# Habilia o socket do servidor a aceitar conexoes. 10 -> numero maximo de conexoes.
+socketDoServidor.listen(10)
 
-# Habilia o servidor a aceitar conexoes, 10 -> numero maximo de conexoes
-servidor.listen(10)
+print("Servidor iniciado.\nIP: " + enderecoDoServidor + "\n" +"Porta: " + str(portaDoServidor))
 
-print("Servidor iniciado.\nIP: " + enderecoIP + "\n" +"Porta: " + str(porta))
-
-# Lista de clientes, inicialmente vazia
+# Lista de (sockets de) clientes, inicialmente vazia.
 listaDeClientes = []
 
-def threadDoCliente(conexao, endereco):
 
-	# Envia uma mensagem ao cliente cujo objeto e conexao
-	conexao.send("Entrou no chat.".encode())
+# Funcoes do servidor
+
+
+def threadDoCliente(socketDoCliente, enderecoDoCliente):
+	# Envia a mensagem ao socket do cliente. 
+	# O metodo send aceita apenas bytes, por isso e necessario o encode.
+	socketDoCliente.send("Entrou no chat.".encode())
 
 	while True:
 		try:
-			# Recebe dados do do socket, 2048 -> buffer size
-			mensagem = conexao.recv(2048) 
+			# Recebe dados do do socket do cliente. 2048 -> buffer size.
+			mensagemRecebida = socketDoCliente.recv(2048) 
 
-			# Ao receber a mensagem
-			if mensagem: 
+			# Ao receber a mensagem.
+			if mensagemRecebida: 
+				# Mostra o endereco do cliente + a mensagem.
+				# A mensagem chega em bytes, por isso e necessario o decode.
+				print("[" + enderecoDoCliente[0] + "]: " + mensagemRecebida.decode()) 
 
-				# Mostra quem mandou a mensagem + a mensagem
-				print("[" + endereco[0] + "]: " + mensagem.decode()) 
+				# Chama a funcao transmitirMensagem para enviar a mensagem para todos os clientes.
+				mensagemASerTransmitida = "[" + enderecoDoCliente[0] + "]: " + mensagemRecebida 
+				transmitirMensagem(mensagemASerTransmitida, socketDoCliente) 
 
-				# Chama a funcao transmitir para enviar a mensagem para todos os clientes
-				mensagemTransmitida = "[" + endereco[0] + "]: " + mensagem 
-				transmitir(mensagemTransmitida, conexao) 
+			else:
+				# Se a mensagem nao houver conteudo, remove o socket do cliente.
+				remove(socketDoCliente)
 
-			else: 
-				# Se a mensagem nao houver conteudo, remove a conexao
-				remove(conn)
-
-		except: 
+		except:
 			continue
 
-# Transmite a mensagem para todos os clientes com objeto diferente do cliente que enviou
-def transmitir(mensagem, conexao):
-	for clientes in listaDeClientes:
-		if clientes!=conexao:
+# Transmite a mensagem para todos os clientes com objetos socket diferente de quem enviou
+def transmitirMensagem(mensagemASerTransmitida, socketDoCliente):
+	for cliente in listaDeClientes:
+		if cliente!=socketDoCliente:
 			try:
-				clientes.send(mensagem.encode())
+				# Envia a mensagem
+				cliente.send(mensagemASerTransmitida.encode())
 			except:
-				clientes.close()
-
-				# Se a conexao estiver quebrada, remove-a
+				# Se a socketDoCliente estiver quebrada, remove-a
+				cliente.close()
 				remove(clients)
 
 # Remove o cliente da listaDeClientes
-def remover(conexao): 
-	if conexao in listaDeClientes: 
-		listaDeClientes.remove(conexao)
+def remover(socketDoCliente): 
+	if socketDoCliente in listaDeClientes: 
+		listaDeClientes.remove(socketDoCliente)
+
+
+# Loop do servidor
+
 
 while True: 
+	# Aceita a conexao e recebe dois retornos: socket do cliente e endereco do cliente.
+	socketDoCliente, enderecoDoCliente = socketDoServidor.accept() 
 
-	# Aceita a conexao e guarda dois parametros
-	# conexao -> objeto socket do cliente
-	# endereco -> endereco IP do cliente
-	conexao, endereco = servidor.accept() 
-
-	# Insere a conexao na lista de clientes
-	listaDeClientes.append(conexao) 
+	# Insere o socket do cliente na lista de clientes
+	listaDeClientes.append(socketDoCliente) 
 
 	# Mostra o endereco do cliente que se conectou
-	print(endereco[0] + " se conectou")
+	print(enderecoDoCliente[0] + " se conectou")
 
 	# Cria uma thread individual para cada cliente
-	_thread.start_new_thread(threadDoCliente,(conexao,endereco))	 
+	_thread.start_new_thread(threadDoCliente,(socketDoCliente, enderecoDoCliente))	 
 
-conexao.close() 
-servidor.close()  
+socketDoCliente.close() 
+socketDoServidor.close()  
